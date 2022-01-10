29d347893ef1d3dd0198492891069e897dc90761
Roda W <93175113+RodaW@users.noreply.github.com>
2022-01-10 18:00:24 -0500

    added seeds

---
+++ db/seeds/quizzes.sql
@@ -0,0 +1,3 @@
+-- Users table seeds
+INSERT INTO Quizzes (id,description,is_public,Users_id) VALUES (1,"quiz title 1",0,1);
+INSERT INTO Quizzes (id,description,is_public,Users_id) VALUES (2,"quiz title 2",0,1);
